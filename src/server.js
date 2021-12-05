e3bf426447ac99919065d70e06b76371a6c14382
roremDev <rorem.94.developer@gmail.com>
2021-12-05 15:28:33 -0600

    docs: addition of jsdoc

--- src/server.js
+++ src/server.js
@@ -9,6 +9,16 @@ server.set("version", version);
 server.set("port", 8080);
 server.set("url", "http://localhost:8080");
 
+/**
+ * @description get cawsay hello world
+ * @param {string} - route definition
+ * @param {function} callback - callback function
+ * @returns {string} - cawsay hello world
+ *
+ * @name callback
+ * @param {object} _ - express request object
+ * @param {object} res - express response object
+ */
 server.get("/", function (_, res) {
   res.setHeader("Content-Type", "text/txt");
   res.end(
@@ -20,15 +30,25 @@ server.get("/", function (_, res) {
   );
 });
 
+/**
+ * @description get cawsay custome message
+ * @param {string} - route definition
+ * @param {function} callback - callback function
+ * @returns {string} - cawsay custome message
+ *
+ * @name callback
+ * @param {object} req - express request object
+ * @param {object} res - express response object
+ */
 server.get("/:message", function (req, res) {
-    res.setHeader("Content-Type", "text/txt");
-    res.end(
-      cowsay.say({
-        text: `${req.params.message}`,
-        e: "oO",
-        T: "U ",
-      })
-    );
-  });
+  res.setHeader("Content-Type", "text/txt");
+  res.end(
+    cowsay.say({
+      text: `${req.params.message}`,
+      e: "oO",
+      T: "U ",
+    })
+  );
+});
 
 module.exports = server;
